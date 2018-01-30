1e545322dc0d990f0155d96215cd5ac24b3075e4
StefMa <StefMaDev@outlook.com>
2018-01-30 14:47:25 +0100

    Working java example

--- buildSrc/src/main/groovy/net/grandcentrix/thirtyinch/Publish.groovy
+++ buildSrc/src/main/groovy/net/grandcentrix/thirtyinch/Publish.groovy
@@ -1,39 +1,25 @@
-//buildscript {
-//    repositories {
-//        jcenter()
-//    }
-//    dependencies {
-//        classpath 'com.novoda:bintray-release:0.8.0'
-//    }
-//}
+package net.grandcentrix.thirtyinch
 
-//package net.grandcentrix.thirtyinch
+import org.gradle.api.Project
+
 /**
-     * Call this method with a closure to configure publishing your thirtyinch module to bintray.
-     */
-//    def configurePublish(Project project, Closure body) {
-//        println project
-//
-//        apply plugin: com.novoda.gradle.release.ReleasePlugin
-//
-//        project.publish {
-//            userOrg = 'grandcentrix'
-//            groupId = 'net.grandcentrix.thirtyinch'
-//            publishVersion = VERSION_NAME
-//            uploadName = 'ThirtyInch'
-//            website = 'https://github.com/grandcentrix/ThirtyInch'
-//            body.delegate = delegate
-//            body()
-//        }
-//    }
-//
-//    static def printHello() {
-//        println "hello from buildSrc"
-//        println project
-//    }
+ * Call this method with a closure to configure publishing your thirtyinch module to bintray.
+ */
+def configurePublish(Project project, Closure body) {
+    println project
+    project.apply([plugin: 'com.novoda.bintray-release'])
 
-//
-//ext {
-//    // expose method to everyone applying this gradle file
-//    configurePublish = this.&configurePublish
-//}
+    project.publish {
+        userOrg = 'grandcentrix'
+        groupId = 'net.grandcentrix.thirtyinch'
+        publishVersion = VERSION_NAME
+        uploadName = 'ThirtyInch'
+        website = 'https://github.com/grandcentrix/ThirtyInch'
+        body.delegate = delegate
+        body()
+    }
+}
+
+static def printHello() {
+    println "hello from buildSrc"
+}
--- buildSrc/src/main/java/net/grandcentrix/thirtyinch/Deps.java
+++ buildSrc/src/main/java/net/grandcentrix/thirtyinch/Deps.java
@@ -1,4 +1,10 @@
-package myapp.gradle;
+package net.grandcentrix.thirtyinch;
+
+import com.novoda.gradle.release.PublishExtension;
+import groovy.lang.Closure;
+import java.util.HashMap;
+import java.util.Map;
+import org.gradle.api.Project;
 
 public class Deps {
 
@@ -15,4 +21,23 @@ public class Deps {
     public static final String constraintLayout = "com.android.support.constraint:constraint-layout:1.0.2";
 
     public static final String junit = "junit:junit:4.12";
+
+    public static void configPublish(Project project, Closure body) {
+        System.out.println(project);
+        final HashMap hashMap = new HashMap<>();
+        hashMap.put("plugin", "com.novoda.bintray-release");
+        project.apply(hashMap);
+
+        final Object publicEx = project.getExtensions().findByName("publish");
+        final PublishExtension extension = (PublishExtension) publicEx;
+        extension.setUserOrg("grandcentrix");
+        extension.setGroupId("net.grandcentrix.thirtyinch");
+        extension.setPublishVersion("1");
+        extension.setWebsite("https://github.com/grandcentrix/ThirtyInch");
+        final Map<String, String> call = (Map<String, String>) body.call();
+        extension.setArtifactId(call.get("artifactId"));
+        extension.setDesc(call.get("desc"));
+        System.out.println(extension.getGroupId());
+    }
+
 }
